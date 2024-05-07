662716e65f916c5d4cb9c871b24e09a9ed62b641
AKANE-NOTEPC\love_ <akn.o.osk@gmail.com>
2024-05-07 20:49:49 +0900

    検索機能（アイコン表示以外）完了

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,13 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
+use App\User;
+
 class UsersController extends Controller
 {
-    //
     public function profile(){
         return view('users.profile');
     }
-    public function search(){
-        return view('users.search');
+    //検索機能実装
+    public function search(Request $request){
+        $users = User::get();
+        $users =  $request->input('username');
+        $keyword = $request->input('keyword');
+
+        if(!empty($keyword)){
+            $users = User::where('username','like', '%'.$keyword.'%')->get();
+        }else{
+            $users = User::where('id','<>', Auth::id())->get();
+            //dd($users); //dd関数の記述を追加
+        }
+        return view('users.search',['users'=>$users],['keyword'=>$keyword]);
     }
 }
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.login')
 
 @section('content')
-        <form action="/search" method="post">
+        <form action="/search" method="get">
         @csrf
         <input type="text" name="keyword" class="form" placeholder="ユーザ名">
         <button type="submit" class="btn btn-success">検索</button>
         </form>
+
+        @foreach ($users as $user)
+        <tr>
+                <td>{{ $user->username }}</td>
+        </tr>
+@endforeach
+
 @endsection
